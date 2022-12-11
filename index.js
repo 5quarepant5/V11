2206fb18837c3282670b538fca1cf14af4535b1d
5quarepant5 <119021507+5quarepant5@users.noreply.github.com>
2022-12-12 06:21:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-
+require("http").createServer((_, res) => res.end("Uptime!")).listen(8080)
 require('./config')
 const { default: makeWASocket, useSingleFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@adiwajshing/baileys")
 const { state, saveState } = useSingleFileAuthState(`./${sessionName}.json`)
